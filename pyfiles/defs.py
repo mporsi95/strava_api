4b0340409e29381e5de3ae60ed0b73c61bb4ea7e
mporsi95 <matheusorsi@gmail.com>
2023-10-08 20:15:40 -0300

    gitignore

--- pyfiles/defs.py
+++ pyfiles/defs.py
@@ -1,9 +1,69 @@
 import requests
 import urllib3
 
-from keys import client_id, client_secret, refresh_token
+from keys import CLIENT_ID, CLIENT_SECRET
+from oauthlib.oauth2 import WebApplicationClient
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+## Authorize Strava API
+def authorize_strava() -> str:
+    '''Authorize Strava API
+    Input: None
+    Output: Authorization URL
+    '''
+    # Set url
+    auth_url = 'https://www.strava.com/oauth/authorize'
+
+    # Set client
+    client = WebApplicationClient(CLIENT_ID)
+
+    # Set authorization url
+    url = client.prepare_request_uri(
+            auth_url,
+            redirect_uri = 'http://localhost:8080',
+            scope = ['activity:read_all'],
+            approval_prompt = 'force'
+    )
+
+    print(f'Siga o link para autorizar e anote o código: \n {url}')
+    code = input('\n Insira o código: ')
+
+    data = client.prepare_request_body(
+                code = code,
+                redirect_uri = 'http://localhost:8080',
+                client_id = CLIENT_ID,
+                client_secret = CLIENT_SECRET
+            )
+
+    # Post request
+    token_url = 'https://www.strava.com/api/v3/oauth/token'
+    response = requests.post(token_url, data=data)
+    
+    # Check for errors
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(e)
+        return None
+
+    # Return token
+    return response.json()['access_token']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## Request token from Strava API
 def request_token() -> dict:
@@ -16,7 +76,7 @@ def request_token() -> dict:
     >>> request_token('114849', 'e27bf5a173ce3bcd101be0b49de75143182147f9', '595b13cadcdced379b4dabb36de42f5ecf5f8e3c')
     '''
     # Set url
-    auth_url = "https://www.strava.com/oauth/token"
+    auth_url = "https://www.strava.com/oauth/authorize"
 
     # Set payload
     payload = {
@@ -24,13 +84,14 @@ def request_token() -> dict:
         'client_secret': client_secret,
         'refresh_token': refresh_token,
         'grant_type': 'refresh_token',
-        'f': 'json'
+        'f': 'json',
+        'scope': 'read_all,profile:read_all,activity:read_all,activity:write'
     }
 
     print("Requesting Token...\n")
 
     # Post request
-    response = requests.post(auth_url, data = payload, verify = False)
+    response = requests.get(auth_url, data = payload, verify = False)
 
     # Check for errors
     try:
@@ -48,7 +109,7 @@ def request_token() -> dict:
 
 
 ## Get data from Strava API
-def get_data_strava(url: str) -> dict:
+def get_data_strava(url: str, access_token: str) -> dict:
     '''Get data from Strava API
     Input: url
     Output: data from Strava API
@@ -59,7 +120,7 @@ def get_data_strava(url: str) -> dict:
     '''
     # Set headers
     headers = {
-        # 'Authorization': f'Bearer {key}'
+        'Authorization': f'Bearer {access_token}'
     }
 
     # Get data from url
--- pyfiles/keys.py
+++ pyfiles/keys.py
@@ -1,4 +1,2 @@
-access_token = '595b13cadcdced379b4dabb36de42f5ecf5f8e3c'
-client_id = '114849'
-client_secret = 'e27bf5a173ce3bcd101be0b49de75143182147f9'
-refresh_token = 'bc1135d980bf841779e196571953a80b3b42df50'
+CLIENT_ID = '114849'
+CLIENT_SECRET = 'e27bf5a173ce3bcd101be0b49de75143182147f9'
